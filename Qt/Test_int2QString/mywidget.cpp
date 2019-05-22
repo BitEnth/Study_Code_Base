ee2aca798a8c22e7629b4353efcb99c1978c4f1d
BitEnth <42052270+BitEnth@users.noreply.github.com>
2019-05-22 12:11:41 +0800

    Add files via upload

---
+++ Qt/Test_int2QString/mywidget.cpp
@@ -0,0 +1,21 @@
+﻿#include "mywidget.h"
+#include "ui_mywidget.h"
+
+MyWidget::MyWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MyWidget)
+{
+    ui->setupUi(this);
+    int i_arr[10];
+    QString i_str;
+    for (int i = 0, j = -3;i < 10;i++, j--) {
+        i_arr[i] = i*j-7;
+        i_str += QString::number(i_arr[i]);
+    }
+    ui->textBrowser->setText(i_str);
+}
+
+MyWidget::~MyWidget()
+{
+    delete ui;
+}
